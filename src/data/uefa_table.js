53e9963ea664b67b18b0ce1a2a59b2229ef1d898
Andij Salamakha <andijslmkh@Andijs-Mac-mini.local>
2024-09-16 00:23:52 +0300

    create file uefa table data

---
+++ src/data/uefa_table.js
@@ -0,0 +1,26 @@
+const LCH = [
+  {
+    id: 1,
+    team: "Real Madrid",
+    position: 1,
+    points: 0,
+  },
+  {
+    id: 2,
+    team: "Arsenal",
+    position: 2,
+    points: 0,
+  },
+  {
+    id: 3,
+    team: "Bayern",
+    position: 3,
+    points: 0,
+  },
+  {
+    id: 4,
+    team: "Milan",
+    position: 4,
+    points: 0,
+  },
+];
