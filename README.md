3a8e8cae0a580323b46e592655e171a61634c2e2
Andij Salamakha <andijslmkh@Andijs-Mac-mini.local>
2024-09-15 23:31:25 +0300

    add gulp

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# football_prediction_24_25
-football prediction 24/25
+# NPM
+
+<p>dev: "gulp"</p>
+<p>tlw: "npx tailwindcss -i ./src/css/output.css -o ./src/css/style.min.css --watch"</p>
+<p>build: "gulp build"</p>
+
+# GULP
+
+<p>exports.styles = styles;</p>
+<p>exports.scripts = scripts;</p>
+<p>exports.watching = watching;</p>
+<p>exports.browser = browser;</p>
+<p>exports.building = building;</p>
+
+<p>exports.build = series(cleanDist, cssCompressed, building);</p>
+<p>exports.default = parallel(styles, scripts, watching, browser);</p>
---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+const gulp = require("gulp");
+const { src, dest, watch, series, parallel } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglify-es").default;
+const sourcemaps = require("gulp-sourcemaps");
+const browserSync = require("browser-sync").create();
+const clean = require("gulp-clean");
+
+function styles() {
+  return src("src/scss/style.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(concat('output.css'))
+    .pipe(dest("src/css"))
+    .pipe(browserSync.stream());
+}
+
+function cssCompressed() {
+  return src("src/css/style.min.css")
+  .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+  .pipe(dest("src/css"))
+}
+
+function scripts() {
+  return src(["src/data/**/*.js", "src/js/*.js", "!src/js/main.min.js"])
+    .pipe(concat("main.min.js"))
+    .pipe(uglify())
+    .pipe(dest("src/js"))
+    .pipe(browserSync.stream());
+}
+
+function watching() {
+  watch(["src/scss/*.scss"], styles);
+  watch(["src/data/**/*.js"], scripts);
+  watch(["src/js/main.js"], scripts);
+  watch(["src/**/*.html"]).on("change", browserSync.reload);
+}
+
+function browser() {
+  browserSync.init({
+    server: {
+      baseDir: "src/",
+    },
+  });
+}
+
+function cleanDist() {
+  return src("dist").pipe(clean());
+}
+
+function building() {
+  return src(["src/css/style.min.css", "src/js/main.min.js", "src/**/*.html"], {
+    base: "src",
+  }).pipe(dest("dist"));
+}
+
+exports.styles = styles;
+exports.scripts = scripts;
+exports.watching = watching;
+exports.browser = browser;
+exports.building = building;
+
+exports.build = series(cleanDist, cssCompressed, building);
+exports.default = parallel(styles, scripts, watching, browser);
