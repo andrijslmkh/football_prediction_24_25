2308a6d94cd8308674c5a108a24a48ac0b827e94
andrijslmkh <andrijsalamakha@gmail.com>
2024-09-17 03:22:39 +0300

    all milanskih prediction on LCH data file

---
+++ src/data/milanskih/milanskih_lch.js
@@ -0,0 +1,182 @@
+const MILANSKIH_LCH = [
+  {
+    id: 1,
+    name: "Arsenal",
+    position: 6,
+  },
+  {
+    id: 2,
+    name: "Aston Villa",
+    position: 16,
+  },
+  {
+    id: 3,
+    name: "Atalanta",
+    position: 20,
+  },
+  {
+    id: 4,
+    name: "Atletico",
+    position: 12,
+  },
+  {
+    id: 5,
+    name: "B. Dortmund",
+    position: 9,
+  },
+  {
+    id: 6,
+    name: "Barcelona",
+    position: 2,
+  },
+  {
+    id: 7,
+    name: "Bayern München",
+    position: 3,
+  },
+  {
+    id: 8,
+    name: "Benfica",
+    position: 19,
+  },
+  // {
+  //   id: 9,
+  //   name: "Bologna",
+  //   position: 9,
+  // },
+  // {
+  //   id: 10,
+  //   name: "Brest",
+  //   position: 10,
+  // },
+  {
+    id: 11,
+    name: "Celtic",
+    position: 24,
+  },
+  // {
+  //   id: 12,
+  //   name: "Club Brugge",
+  //   position: 12,
+  // },
+  // {
+  //   id: 13,
+  //   name: "Crvena Zvezda",
+  //   position: 13,
+  // },
+  // {
+  //   id: 14,
+  //   name: "Feyenoord",
+  //   position: 14,
+  // },
+  {
+    id: 15,
+    name: "Girona",
+    position: 14,
+  },
+  // {
+  //   id: 16,
+  //   name: "GNK Dinamo",
+  //   position: 16,
+  // },
+  {
+    id: 17,
+    name: "Inter",
+    position: 8,
+  },
+  // {
+  //   id: 18,
+  //   name: "Juventus",
+  //   position: 18,
+  // },
+  {
+    id: 19,
+    name: "Leipzig",
+    position: 15,
+  },
+  {
+    id: 20,
+    name: "Leverkusen",
+    position: 10,
+  },
+  // {
+  //   id: 21,
+  //   name: "Lille",
+  //   position: 21,
+  // },
+  {
+    id: 22,
+    name: "Liverpool",
+    position: 7,
+  },
+  {
+    id: 23,
+    name: "Man City",
+    position: 1,
+  },
+  {
+    id: 24,
+    name: "Milan",
+    position: 11,
+  },
+  {
+    id: 25,
+    name: "Monaco",
+    position: 17,
+  },
+  {
+    id: 26,
+    name: "Paris",
+    position: 5,
+  },
+  {
+    id: 27,
+    name: "PSV",
+    position: 18,
+  },
+  {
+    id: 28,
+    name: "Real Madrid",
+    position: 4,
+  },
+  // {
+  //   id: 29,
+  //   name: "S. Bratislava",
+  //   position: 29,
+  // },
+  {
+    id: 30,
+    name: "Salzburg",
+    position: 23,
+  },
+  {
+    id: 31,
+    name: "Shakhtar",
+    position: 22,
+  },
+  // {
+  //   id: 32,
+  //   name: "Sparta Praha",
+  //   position: 32,
+  // },
+  {
+    id: 33,
+    name: "Sporting CP",
+    position: 21,
+  },
+  // {
+  //   id: 34,
+  //   name: "Sturm Graz",
+  //   position: 34,
+  // },
+  {
+    id: 35,
+    name: "Stuttgart",
+    position: 13,
+  },
+  // {
+  //   id: 36,
+  //   name: "Young Boys",
+  //   position: 36,
+  // },
+];
