736f91c60c7f48f712265337cf07ec1fd3124aae
andrijslmkh <andrijsalamakha@gmail.com>
2024-09-19 23:36:02 +0300

    update newObjectCbSort fun

--- src/js/hook/newObjectCbSort.js
+++ src/js/hook/newObjectCbSort.js
@@ -1,14 +1,18 @@
 function newObjectCbSort(obj, cb) {
-  let sortObj = {};
-
-  for (const key in obj) {
-    sortObj[key] = [];
-
-    obj[key].forEach((element) => {
-      sortObj[key].push(element);
-    });
+  let newObj = {};
+  for (const firstKey in obj) {
+    newObj[firstKey] = {};
+    for (const name in obj[firstKey]) {
+      let arr = [];
+      obj[firstKey][name].forEach((el) => {
+        arr.push(el);
+      });
+      newObj[firstKey][name] = [...arr];
+    }
+    cb !== undefined
+      ? (newObj[firstKey] = cb(newObj[firstKey]))
+      : newObj[firstKey];
   }
 
-  cb !== undefined ? (sortObj = cb(sortObj)) : sortObj[key];
-  return sortObj;
+  return newObj;
 }
