682ab56adaa33b6d0191fede0770cd31b0bf2f80
andrijslmkh <andrijsalamakha@gmail.com>
2024-11-03 13:47:38 +0200

    add removeClass and change name addClass to addClassFromId

--- src/js/hook/colorsFunction.js
+++ src/js/hook/colorsFunction.js
@@ -5,11 +5,16 @@ const tirOneTo = 8;
 const tirTwoWith = 9;
 const tirTwoTo = 24;
 
-function addClassList(elements, id, bg) {
+function addClassFromId(elements, id, bg) {
   elements.forEach((el) => {
     if (el.getAttribute("data-id") == id) {
       el.classList.add(bg);
     }
+  });
+}
+function removeClass(elements, bg) {
+  elements.forEach((el) => {
+    el.classList.remove(bg);
   });
 }
 
@@ -26,9 +31,9 @@ function colors({ data }, userPred, league) {
       if (dtUser[i].id === dt[k].id) {
         // 3 point
         if (dtUser[i].position === dt[k].position) {
-          addClassList(predIconBlock, dtUser[i].id, bgGreen);
-          addClassList(icon, dtUser[i].id, bgGreen);
-          addClassList(team, dtUser[i].id, bgGreen);
+          addClassFromId(predIconBlock, dtUser[i].id, bgGreen);
+          addClassFromId(icon, dtUser[i].id, bgGreen);
+          addClassFromId(team, dtUser[i].id, bgGreen);
         }
         // 1 point
         if (dtUser[i].position !== dt[k].position) {
@@ -38,9 +43,9 @@ function colors({ data }, userPred, league) {
             dt[k].position >= tirOneWith &&
             dt[k].position <= tirOneTo
           ) {
-            addClassList(predIconBlock, dtUser[i].id, bgYellow);
-            addClassList(icon, dtUser[i].id, bgYellow);
-            addClassList(team, dtUser[i].id, bgYellow);
+            addClassFromId(predIconBlock, dtUser[i].id, bgYellow);
+            addClassFromId(icon, dtUser[i].id, bgYellow);
+            addClassFromId(team, dtUser[i].id, bgYellow);
           }
           if (
             dtUser[i].position >= tirTwoWith &&
@@ -48,9 +53,9 @@ function colors({ data }, userPred, league) {
             dt[k].position >= tirTwoWith &&
             dt[k].position <= tirTwoTo
           ) {
-            addClassList(predIconBlock, dtUser[i].id, bgYellow);
-            addClassList(icon, dtUser[i].id, bgYellow);
-            addClassList(team, dtUser[i].id, bgYellow);
+            addClassFromId(predIconBlock, dtUser[i].id, bgYellow);
+            addClassFromId(icon, dtUser[i].id, bgYellow);
+            addClassFromId(team, dtUser[i].id, bgYellow);
           }
         }
       }
@@ -63,16 +68,23 @@ function clearColors() {
   let icon = document.querySelectorAll(".team__icon");
   let team = document.querySelectorAll(".team__name");
 
-  predIconBlock.forEach((el) => {
-    el.classList.remove(bgGreen);
-    el.classList.remove(bgYellow);
-  });
-  icon.forEach((el) => {
-    el.classList.remove(bgGreen);
-    el.classList.remove(bgYellow);
-  });
-  team.forEach((el) => {
-    el.classList.remove(bgGreen);
-    el.classList.remove(bgYellow);
-  });
+  removeClass(predIconBlock, bgGreen);
+  removeClass(predIconBlock, bgYellow);
+  removeClass(icon, bgGreen);
+  removeClass(icon, bgYellow);
+  removeClass(team, bgGreen);
+  removeClass(team, bgYellow);
+
+  // predIconBlock.forEach((el) => {
+  //   el.classList.remove(bgGreen);
+  //   el.classList.remove(bgYellow);
+  // });
+  // icon.forEach((el) => {
+  //   el.classList.remove(bgGreen);
+  //   el.classList.remove(bgYellow);
+  // });
+  // team.forEach((el) => {
+  //   el.classList.remove(bgGreen);
+  //   el.classList.remove(bgYellow);
+  // });
 }
