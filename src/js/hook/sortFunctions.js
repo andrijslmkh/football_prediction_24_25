e34bd84baacc80b6b26fddc2e27cd66c244c8d46
andrijslmkh <andrijsalamakha@gmail.com>
2024-12-16 23:15:18 +0200

    fix

--- src/js/hook/sortFunctions.js
+++ src/js/hook/sortFunctions.js
@@ -8,7 +8,7 @@ function objSortPosition(obj) {
 }
 
 function arrSortTotal(arr) {
-  let result = arr.slice();
+  let result = [...arr];
   result.sort((a, b) => b.total - a.total);
   return result;
 }
